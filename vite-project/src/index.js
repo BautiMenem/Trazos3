e4023822725b144f2e1fd79dc91a8813963dd7b4
Bautista Menem <112192354+BautiMenem@users.noreply.github.com>
2025-08-06 15:43:33 -0300

    first commit

--- vite-project/src/index.js
+++ vite-project/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from 'vite-project\src\App.jsx';
+import App from 'vite-project/src/App.jsx';
 import './index.css'; // opcional si usás estilos globales
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
